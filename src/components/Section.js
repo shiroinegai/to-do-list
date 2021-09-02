d3e292a2ede9596c7f1eea3db3cb26e2b193bf6d
Shiroi Negai <shiroinegai@pm.me>
2021-09-02 08:16:29 +0800

    Implement Section CRUD methods
    Add fetchSections and findSectionById

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,7 +1,10 @@
 import { v4 as uuidv4 } from "uuid";
+import { findProjectById } from "./Project";
 
 /**
  * Section factory function
+ * @private
+ * @function newSection
  * @param {string} projectId
  * @param {string} name
  * @returns {Section} Section object
@@ -13,20 +16,86 @@ const newSection = (projectId, name = "") => {
   return { id, projectId, name, tasks };
 };
 
-const createSection = () => {
-  //TODO
+/**
+ * @public
+ * @function createSection
+ * @param {string} projectIdQuery
+ * @param {string} name
+ */
+const createSection = (projectIdQuery, name) => {
+  const section = newSection(projectIdQuery, name);
+  const targetProject = findProjectById(projectIdQuery);
+  targetProject.sections.push(section);
 };
 
-const readSection = () => {
-  //TODO
+/**
+ * @public
+ * @function readSection
+ * @param {string} projectIdQuery
+ * @param {string} sectionIdQuery
+ * @returns {Section}
+ */
+const readSection = (projectIdQuery, sectionIdQuery) => {
+  return findSectionById(projectIdQuery, sectionIdQuery);
 };
 
-const updateSection = () => {
-  //TODO
+/**
+ * @public
+ * @function updateSection
+ * @param {string} projectIdQuery
+ * @param {string} sectionIdQuery
+ * @param {Section} sectionUpdates
+ */
+const updateSection = (projectIdQuery, sectionIdQuery, sectionUpdates) => {
+  let section = findSectionById(projectIdQuery, sectionIdQuery);
+  section = { ...section, ...sectionUpdates };
 };
 
-const deleteSection = () => {
-  //TODO
+/**
+ * @public
+ * @function deleteSection
+ * @param {string} projectIdQuery
+ * @param {string} sectionIdQuery
+ */
+const deleteSection = (projectIdQuery, sectionIdQuery) => {
+  const targetProject = findProjectById(projectIdQuery);
+  targetProject.sections = targetProject.sections.filter(
+    (section) => section.id !== sectionIdQuery
+  );
 };
 
-export { newSection, createSection, readSection, updateSection, deleteSection };
+/**
+ * @public
+ * @function fetchSections
+ * @param {string} projectIdQuery
+ * @returns {Section[]}
+ */
+function fetchSections(projectIdQuery) {
+  return findProjectById(projectIdQuery).sections;
+}
+
+/**
+ * @public
+ * @function findSectionById
+ * @param {string} projectIdQuery
+ * @param {string} sectionIdQuery
+ * @returns {Section}
+ */
+function findSectionById(projectIdQuery, sectionIdQuery) {
+  const targetProject = findProjectById(projectIdQuery);
+  for (let i = 0; i < targetProject.sections.length; i++) {
+    const section = targetProject.sections[i];
+    if (section.id === sectionIdQuery) {
+      return section;
+    }
+  }
+}
+
+export {
+  createSection,
+  readSection,
+  updateSection,
+  deleteSection,
+  fetchSections,
+  findSectionById,
+};
