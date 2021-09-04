77dfb3769f106ff43ab7a08718a5630adad70426
Shiroi Negai <shiroinegai@pm.me>
2021-09-04 21:18:58 +0800

    Style MainNav
    Make various style changes to begin work on mobile-first interface

--- src/components/interface/MainNav.js
+++ src/components/interface/MainNav.js
@@ -1,19 +1,24 @@
 import { menuIcon, homeIcon, quickTaskIcon, userProfileIcon } from "./Icons";
 
-const TopNav = document.createElement("nav");
+const MainNav = document.createElement("nav");
+MainNav.classList.add("c-MainNav");
 
 const menu = document.createElement("button");
+menu.classList.add("c-MainNav__button");
 menu.append(menuIcon);
 
 const home = document.createElement("button");
+home.classList.add("c-MainNav__button");
 home.append(homeIcon);
 
 const quickTask = document.createElement("button");
+quickTask.classList.add("c-MainNav__button");
 quickTask.append(quickTaskIcon);
 
 const userProfile = document.createElement("button");
+userProfile.classList.add("c-MainNav__button");
 userProfile.append(userProfileIcon);
 
-TopNav.append(menu, home, quickTask, userProfile);
+MainNav.append(menu, home, quickTask, userProfile);
 
-export default TopNav;
+export default MainNav;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import "./styles/main.scss";
 
-import TopNav from "./components/interface/TopNav";
+import MainNav from "./components/interface/MainNav";
 import Menu from "./components/interface/Menu";
 import Main from "./components/interface/Main";
 
 const root = document.getElementById("root");
-root.append(TopNav, Menu, Main);
+root.append(MainNav, Menu, Main);
 
 console.log("To Do List is running.");
