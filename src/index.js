72de2a83b1a9a2e9f1643432d567b43f7e0962a3
Shiroi Negai <shiroinegai@pm.me>
2021-09-08 04:20:35 +0800

    Clean up src/index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-import { indexSections } from "./components/Section";
-import { fetchAllSections } from "./components/Section/api";
-import { getTaskIndex, indexTasks } from "./components/Task";
-import { updateTask } from "./components/Task/api";
-import fetchAllTasks from "./components/Task/api/fetchAllTasks";
 import "./styles/main.scss";
 
 console.log("To Do List is running.");
-
-indexSections();
-
-updateTask("welcome-t0-t0", { header: "UPDATED HEADER" });
-
-console.log(fetchAllSections());
-
-indexTasks();
-
-console.log(fetchAllTasks());
